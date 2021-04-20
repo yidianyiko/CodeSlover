9e652fe45aba8fb7135da045c7b10aac1054534d
YDYK <yidianyiko@foxmail.com>
2021-04-20 10:31:23 +0800

    add  tree

---
+++ stack & queue/滑动窗口最大值.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        queue<int> qe;
+        
+        for(int i = 0; i <nums.size();i+=3){
+            
+        }
+    }
+};
